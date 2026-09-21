6564156fbf814f36a5d5dbc4ddf26dedec76f4db
Angel Velasquez <pcisavel@upc.edu.pe>
2026-09-21 18:16:43 -0500

    feat(main): add layout component with navigation and drawer support

---
+++ src/shared/presentation/components/layout.vue
@@ -0,0 +1,44 @@
+<script setup>
+import {useI18n} from "vue-i18n";
+import {ref} from "vue";
+import {ConfirmDialog as PvConfirmDialog, Toast as PvToast, Toolbar as PvToolbar} from "primevue";
+
+const { t } = useI18n();
+const drawer = ref(false);
+const toggleDrawer = () => drawer.value = !drawer.value;
+
+const items = [
+  { label: 'option.home', to: '/home' },
+  { label: 'option.about', to: '/about' }
+];
+</script>
+
+<template>
+  <pv-toast/>
+  <pv-confirm-dialog/>
+  <header class="absolute top-0 left-0 w-full">
+    <pv-toolbar class="bg-primary">
+      <template #start>
+        <pv-button class="p-button-text" icon="pi pi-bars" @click="toggleDrawer"/>
+        <h3>ACME Learning Center</h3>
+      </template>
+      <template #end>
+        <div class="flex-column mr-3">
+          <pv-button v-for="item in items" :key="item.label" as-child v-slot="slotProps">
+            <router-link :to="item.to" :class="slotProps['class']">{{t(item.label)}}</router-link>
+          </pv-button>
+        </div>
+        <!-- To add when IAM is implemented -->
+        <!--<authentication-section/>-->
+        <!-- To add when language-switcher is implemented -->
+        <!--<language-switcher/>-->
+      </template>
+    </pv-toolbar>
+    <pv-drawer v-model:visible="drawer"/>
+  </header>
+  <main class="mt-7">
+    <router-view/>
+  </main>
+  <!-- To add when footer-content is implemented -->
+  <!-- <footer-content/>-->
+</template>
