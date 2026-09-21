4f8d73a6ed0ecb4b4d558e359662c81d8e1ad82c
Angel Velasquez <pcisavel@upc.edu.pe>
2026-09-21 16:55:13 -0500

    feat(in-app-navigation): add In-app navigation router

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './app.vue'
 import i18n from "./i18n.js";
+import router from "./router.js";
 
 createApp(App)
     .use(i18n)
+    .use(router)
     .mount('#app')
