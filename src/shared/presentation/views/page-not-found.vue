74d2c6301a2435675101342a77b101f493388cac
Angel Velasquez <pcisavel@upc.edu.pe>
2026-09-21 17:53:42 -0500

    feat(page-not-found): add Page Not Found view component

---
+++ src/shared/presentation/views/page-not-found.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {useI18n} from "vue-i18n";
+import {useRoute} from "vue-router";
+
+const { t } = useI18n();
+const route = useRoute();
+const unavailableRoute = route.path;
+</script>
+
+<template>
+  <section class="pt-6 p-4 md:p-5">
+    <div class="flex flex-column gap-3">
+      <h1 class="text-4xl font-bold text-color">{{t('page-not-found.title')}}</h1>
+      <p class="m-0 line-height-3 text-color-secondary">
+        {{t('page-not-found.content', { 'unavailable-content': unavailableRoute })}}
+      </p>
+      <router-link to="/home" class="text-primary font-medium">
+        {{t('page-not-found.go-home')}}
+      </router-link>
+    </div>
+  </section>
+</template>
