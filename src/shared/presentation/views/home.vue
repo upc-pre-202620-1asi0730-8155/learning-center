f3549a10c7600c090b297a1ef70591c1001feaa5
Angel Velasquez <pcisavel@upc.edu.pe>
2026-09-21 17:45:36 -0500

    feat(home): add Home view component

--- src/shared/presentation/views/home.vue
+++ src/shared/presentation/views/home.vue
@@ -1,11 +1,14 @@
 <script setup>
+import {useI18n} from "vue-i18n";
 
+const { t } = useI18n();
 </script>
 
 <template>
-
+  <section class="pt-6 p-4 md:p-5">
+    <div class="flex flex-column gap-3">
+      <h1 class="text-4xl font-bold text-color">{{t('home.title')}}</h1>
+      <p class="m-0 line-height-3 text-color-secondary">{{t('home.content')}}</p>
+    </div>
+  </section>
 </template>
-
-<style scoped>
-
-</style>
