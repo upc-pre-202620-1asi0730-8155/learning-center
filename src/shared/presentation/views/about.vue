ba89813a6937de1a43d0183e49d219a6befdd57c
Angel Velasquez <pcisavel@upc.edu.pe>
2026-09-21 17:47:29 -0500

    feat(about): add About Us view component

---
+++ src/shared/presentation/views/about.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {useI18n} from "vue-i18n";
+
+const { t } = useI18n();
+
+</script>
+
+<template>
+  <section class="pt-6 p-4 md:p-5">
+    <div class="flex flex-column gap-3">
+      <h1 class="text-4xl font-bold text-color">{{t('about.title')}}</h1>
+      <p class="m-0 line-height-3 text-color-secondary">{{t('about.content')}}</p>
+    </div>
+  </section>
+
+</template>
